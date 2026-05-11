c7bc8bfed127c13b36e03b034df4663e25c93a6c
ShoxjahonToxtaboyev <nodirbekmatyakubov050@gmail.com>
2026-05-12 03:37:14 +0500

    NOV

---
+++ men/main.js
@@ -0,0 +1,24 @@
+let son1 = 15
+let son2 = 10
+let son3 = 17
+let qoshish = son1 + son2 + son3
+console.log(qoshish)
+let ayrsh = son1 - son2 - son3
+console.log(ayrsh)
+let kopaytirish = son1 * son2 * son3
+console.log(kopaytirish)
+let bolish = son1 / son2 /son3
+console.log(bolish)
+let daraja = son1 ** son2
+let daraja1 = son2 ** son3
+console.log(daraja, daraja1)
+let qoldiq = son1 % son2
+let qoldiq1 = son2 % son3
+console.log (qoldiq)
+console.log( son1 == son2)
+console.log( son2 == son3)
+console.log( son1 === son2 )
+console.log( son2 === son3 )
+
+console.log(son1 < son2)
+
